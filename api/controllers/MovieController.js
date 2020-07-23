fb96fd874d6a17593cba2ac6f6100b0f0131a56e
SaeedPooyanfar <saeedp47@gmail.com>
2020-07-23 16:36:27 +0430

    modify movie controller

--- api/controllers/MovieController.js
+++ api/controllers/MovieController.js
@@ -23,78 +23,14 @@ const MovieController = {
       });
       const data = domData.movie;
       //
-      const watchHtml = await sails.helpers.filimo.with({
-        method: 'get',
-        path: `/w/${data.id}`,
-        requestHeaders: req.headers,
-        responseType: 'text'
-      });
-      //
-      const matches = [...watchHtml.data.matchAll(/var player_data = (.*);/g)];
-      if (typeof matches[0] === 'undefined' || typeof matches[0][1] === 'undefined' || !matches[0][1]) {
-        return res.json({
-          status: 406,
-          success: false,
-          message: 'Details not found',
-          data: {}
-        });
-      }
-      let playerData;
-      try {
-        playerData = JSON.parse(matches[0][1]);
-      } catch (unused) {
-        playerData = {};
-      }
-      //
-      let stream = '';
-      const multiSRC = playerData.multiSRC || [];
-      multiSRC.forEach((item) => {
-        item.forEach((source) => {
-          if (!stream && source.type === 'application/vnd.apple.mpegurl') {
-            stream = source.src;
-          }
-        });
-      });
-      if (!stream || stream.indexOf('http') !== 0) {
-        return res.json({
-          status: 406,
-          success: false,
-          message: 'Stream not found',
-          data: {}
-        });
-      }
-      //
-      const streamFile = await sails.helpers.filimo.with({
-        method: 'get',
-        path: (new URL(stream)).pathname,
-        requestHeaders: req.headers,
-        responseType: 'text'
-      });
-      const variants = [];
-      const variantMatches = [...streamFile.data.matchAll(/#([0-9]+(.*?))\n(.*)RESOLUTION=(.*)\n(.*)/g)];
-      variantMatches.forEach((variant) => {
-        variants.push({
-          quality: variant[1] || '',
-          resolution: variant[4] ? variant[4].split(',')[0] : '',
-          link: variant[5] || ''
-        });
-      });
-      //
-      let subtitle = '';
-      const tracks = playerData.tracks || [];
-      tracks.forEach((track) => {
-        if (!subtitle && track.srclang === 'fa') {
-          subtitle = track.src;
+      data.download = null;
+      if (data.id && data.id !== 'w') {
+        if (data.id !== id) {
+          data.originalId = id;
         }
-      });
-      //
-      if (data.id !== id) {
-        data.originalId = id;
+        data.download = await MovieController.getDownload(data.id, req.headers);
       }
-      data.download = {
-        subtitle,
-        variants
-      };
+      //
       return res.json({
         status: movieHtml.status || 0,
         success: true,
@@ -109,6 +45,66 @@ const MovieController = {
         data: error
       });
     }
+  },
+  getDownload: async (id, requestHeaders) => {
+    const watchHtml = await sails.helpers.filimo.with({
+      method: 'get',
+      path: `/w/${id}`,
+      requestHeaders,
+      responseType: 'text'
+    });
+    //
+    const matches = [...watchHtml.data.matchAll(/var player_data = (.*);/g)];
+    if (typeof matches[0] === 'undefined' || typeof matches[0][1] === 'undefined' || !matches[0][1]) {
+      return null;
+    }
+    let playerData;
+    try {
+      playerData = JSON.parse(matches[0][1]);
+    } catch (unused) {
+      playerData = {};
+    }
+    //
+    let stream = '';
+    const multiSRC = playerData.multiSRC || [];
+    multiSRC.forEach((item) => {
+      item.forEach((source) => {
+        if (!stream && source.type === 'application/vnd.apple.mpegurl') {
+          stream = source.src;
+        }
+      });
+    });
+    if (!stream || stream.indexOf('http') !== 0) {
+      return null;
+    }
+    //
+    const streamFile = await sails.helpers.filimo.with({
+      method: 'get',
+      path: (new URL(stream)).pathname,
+      requestHeaders,
+      responseType: 'text'
+    });
+    const variants = [];
+    const variantMatches = [...streamFile.data.matchAll(/#([0-9]+(.*?))\n(.*)RESOLUTION=(.*)\n(.*)/g)];
+    variantMatches.forEach((variant) => {
+      variants.push({
+        quality: variant[1] || '',
+        resolution: variant[4] ? variant[4].split(',')[0] : '',
+        link: variant[5] || ''
+      });
+    });
+    //
+    let subtitle = '';
+    const tracks = playerData.tracks || [];
+    tracks.forEach((track) => {
+      if (!subtitle && track.srclang === 'fa') {
+        subtitle = track.src;
+      }
+    });
+    return {
+      subtitle,
+      variants
+    };
   }
 };
 
