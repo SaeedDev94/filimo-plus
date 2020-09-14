6248fef87e17bc3409cb4f409bc6eaa8414c84ab
SaeedPooyanfar <saeedp47@gmail.com>
2020-09-14 20:48:08 +0430

    serve static files + enable cors + filimo-plus script in package.json

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
-  NestFastifyApplication
+  NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
+  app.useStaticAssets({
+    root: join(process.cwd(), 'public'),
+    index: ['index.html'],
+  });
+  app.enableCors({
+    origin: (requestOrigin, callback) => {
+      if (process.env.NODE_ENV === 'production') {
+        callback(new Error('Not allowed'), false);
+        return;
+      }
+      let hostname: string;
+      try {
+        hostname = new URL(requestOrigin).hostname;
+      } finally {
+        if (hostname === 'localhost') {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed'), false);
+        }
+      }
+    },
+  });
   await app.listen(1399);
 }
+
 bootstrap();
