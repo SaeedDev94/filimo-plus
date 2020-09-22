6856af2ed6d8e85c67e603095590ababcb8c312d
SaeedPooyanfar <saeedp47@gmail.com>
2020-09-22 20:51:24 +0330

    HomeController methods

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -1,11 +1,31 @@
-import { Controller, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { WrapResponseInterceptor } from '../shared/wrap-response.interceptor';
+import { DomService } from '../dom/dom.service';
+import { IHome } from '../dom/dom.interface';
+import { URL } from 'url';
 
 @Controller('home')
 export class HomeController {
+
+  constructor(
+    private domService: DomService
+  ) {
+  }
+
   @Post('index')
   @UseInterceptors(WrapResponseInterceptor)
-  index() {
-    return 'Test';
+  async index(): Promise<IHome> {
+    const html = await this.domService.getHtml('/');
+    if (!this.domService.authenticated(html)) {
+      throw new Error('Unauthorized');
+    }
+    return this.domService.home(html, true);
+  }
+
+  @Post('next')
+  @UseInterceptors(WrapResponseInterceptor)
+  async next(@Body() body: {next: string}): Promise<IHome> {
+    const html = await this.domService.getHtml(new URL(body.next).pathname);
+    return this.domService.home(html, false);
   }
 }
--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HomeController } from './home.controller';
+import { DomModule } from '../dom/dom.module';
 
 @Module({
+  imports: [
+    DomModule
+  ],
   controllers: [
     HomeController
   ]
